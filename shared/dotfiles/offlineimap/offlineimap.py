dc359f198fc4d12d95f98c74e828bf8c8936bcc9
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-03-15 14:15:37 -0600

    Updated my offlineimap config to dry it up a little

--- shared/dotfiles/offlineimap/offlineimap.py
+++ shared/dotfiles/offlineimap/offlineimap.py
@@ -2,6 +2,33 @@
 import subprocess
 import re
 
+class NameMapping:
+  def __init__(self, local_name, remote_name):
+    self.local_name = local_name
+    self.remote_name = remote_name
+
+class LocalName:
+  def __init__(self, folder):
+    self.folder = folder
+
+  def matches(self, mapping):
+    return mapping.remote_name == self.folder
+
+  def mapped_folder_name(self, mapping):
+    return mapping.local_name
+  
+class RemoteName:
+  def __init__(self, folder):
+    self.folder = folder
+
+  def matches(self, mapping):
+    return mapping.local_name == self.folder
+
+  def mapped_folder_name(self, mapping):
+    return mapping.remote_name
+
+
+
 def get_password(account):
   program ='/Users/jp/repositories/developwithpassion/devtools/automation/keychain/get_keychain_password'
 
@@ -11,8 +38,16 @@ def get_password(account):
 
   return output.rstrip()
 
-def exclusion_patterns():
-  return [
+
+def is_included(folder):
+  result = True
+
+  for pattern in exclusion_patterns:
+    result = result and (re.search(pattern, folder) == None)
+
+  return result
+
+exclusion_patterns = [
   "efax",
   "earth_class_mail",
   "eventbrite",
@@ -33,14 +68,35 @@ def exclusion_patterns():
   "webinars",
   "Gmail.*Spam",
   "Gmail.*Important"
-  ]
+]
 
-def is_excluded(folder):
-  result = False
+name_mappings = [
+  NameMapping('sent', '[Gmail]/Sent Mail'),
+  NameMapping('flagged', '[Gmail]/Starred'),
+  NameMapping('trash',   '[Gmail]/Trash'),
+  NameMapping('archive', '[Gmail]/All Mail'),
+  NameMapping('drafts', '[Gmail]/Drafts')
+]
 
-  for pattern in exclusion_patterns():
-    result = result or (re.search(pattern, folder) != None)
-
-  return result
 
 
+def find_name_mapping(name):
+  default_mapping = NameMapping(name.folder, name.folder)
+
+  for mapping in name_mappings:
+    if (name.matches(mapping)):
+      return mapping
+
+  return default_mapping
+
+def get_name_mapping(name):
+  mapping = find_name_mapping(name)
+  return name.mapped_folder_name(mapping)
+
+def get_remote_name(local_folder_name):
+  name = RemoteName(local_folder_name)
+  return get_name_mapping(name)
+
+def get_local_name(remote_folder_name):
+  name = LocalName(remote_folder_name)
+  return get_name_mapping(name)
